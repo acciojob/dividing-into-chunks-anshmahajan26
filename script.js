05d876dd805f97c946dd95a3a910830c7b60fe15
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-11 16:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  // Write your code her
+	for(let i =0; i<arr.length; i++){
+		for(let j =1; j<arr.length; j++){
+			let sum =0;
+			for(let k=i; k<j; k++){
+				sum += arr[k];
+				if(sum <n){
+					console.log(arr[k]);
+				}
+			} 
+		}
+	}
 };
 
 const n = prompt("Enter n: ");
